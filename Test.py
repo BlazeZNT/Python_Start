13488cbbe98e25922bef91cf1370d99e5ed6003c
Zar Ni Tun <zarnitunkt@gmail.com>
2024-01-27 20:31:04 +0630

    Initialize files

---
+++ Test.py
@@ -0,0 +1,15 @@
+
+snack = ["a","b","c"]
+
+for x in snack:
+    print (f"This is you snack number {x}")
+    
+    
+# val = 'Geeks'
+# print(f"{val}for{val} is a portal for {val}.")
+
+def add_num(a,b):
+    new_num = a+b
+    print(new_num)
+    
+add_num(2,5)
