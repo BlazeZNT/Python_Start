ec2b4b805249aa2eb1fa83eb6a21971d7a01dcce
Zar Ni Tun <zarnitunkt@gmail.com>
2024-02-08 23:02:53 +0630

    Add text file

--- Test.py
+++ Test.py
@@ -1,15 +1,15 @@
 
-snack = ["a","b","c"]
+# snack = ["a","b","c"]
 
-for x in snack:
-    print (f"This is you snack number {x}")
+# for x in snack:
+#     print (f"This is you snack number {x}")
     
     
-# val = 'Geeks'
-# print(f"{val}for{val} is a portal for {val}.")
+# # val = 'Geeks'
+# # print(f"{val}for{val} is a portal for {val}.")
 
-def add_num(a,b):
-    new_num = a+b
-    print(new_num)
+# def add_num(a,b):
+#     new_num = a+b
+#     print(new_num)
     
-add_num(2,5)
+# add_num(2,5)
